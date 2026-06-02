c96f52a0c64c87435c10cdbe0d14ebe38cdfcf1d
Manan Kapkar <85684429+manan75@users.noreply.github.com>
2026-06-02 12:50:56 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> ans;
+        vector<pair<int,int>> pos(26, {-1, -1});
+        for (int i = 0; i < s.size(); i++) {
+            int p = s[i] - 'a';
+            if (pos[p].first == -1)
+                pos[p].first = i;
+            pos[p].second = i;
+        }
+        int i = 0;
+        while (i < s.size()) {
+            int last = pos[s[i] - 'a'].second;
+            for (int j = i + 1; j <= last; j++) {
+                last = max(last, pos[s[j] - 'a'].second);
+            }
+            ans.push_back(last - i + 1);
+            i = last + 1;
+        }
+        return ans;
+    }
+};
